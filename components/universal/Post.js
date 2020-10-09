84f740a0407a578317a7c9e41fb6dc7884342913
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-09 14:48:35 -0400

    forgot to run prettier

--- components/universal/Post.js
+++ components/universal/Post.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const Post = React.memo(({date, title, id}) => <StyledPost></StyledPost>);
+const Post = React.memo(({ date, title, id }) => <StyledPost></StyledPost>);
 
 const StyledPost = styled.div``;
 
 Post.propTypes = {
   date: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired
+  id: PropTypes.number.isRequired,
 };
 
 export default Post;
