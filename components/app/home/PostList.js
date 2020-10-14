10246c624646172b3e236e90208f5f085efd5e54
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-14 19:31:07 -0400

    commented out get static props until I get the back end finished for the home page

--- components/app/home/PostList.js
+++ components/app/home/PostList.js
@@ -1,10 +1,23 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import Post from '../../universal/Post';
 
-const PostList = React.memo(({}) => <StyledPostList></StyledPostList>);
+const PostList = React.memo(({ posts }) => (
+  <StyledPostList>
+    {posts.map(({ date, id, title }) => (
+      <ListItem key={id}>
+        <Post date={date} id={id} title={title} />
+      </ListItem>
+    ))}
+  </StyledPostList>
+));
 
 const StyledPostList = styled.ol``;
 
-PostList.propTypes = {};
+const ListItem = styled.li``;
+
+PostList.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+};
 
 export default PostList;
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,57 @@
 import Head from 'next/head';
+import { useState } from 'react';
 import styled from 'styled-components';
-import { H1 } from '../components/universal';
+import PostList from '../components/app/home/PostList';
+import { H1, P } from '../components/universal';
 import postsService from '../services/postsService';
 
 export default function Home({ posts }) {
+  const fakePosts = [];
+  const [currentPage, setCurrentPage] = useState(0);
+  const currentPosts = fakePosts.slice(currentPage * 5, (currentPage + 1) * 5);
+
   return (
     <HomePageContainer>
-      <Head>Home</Head>
-      <H1>Sean McQuaid's Code Blog</H1>
+      <Head>Home</Head>;
+      <Header>
+        <H1>Sean McQuaid's Code Blog</H1>
+        <P>
+          Welcome to my blog! My name is Sean and I am currently a React
+          Developer at Chick-fil-A. I will primarily post about coding best
+          practices, general advice and how to successfully transition into the
+          industry!
+        </P>
+        <PostList posts={currentPosts} />
+      </Header>
     </HomePageContainer>
   );
 }
 
-export async function getStaticProps() {
-  const posts = await postsService.getPosts();
-  return {
-    props: {
-      posts,
-    },
-  };
-}
+// export async function getStaticProps() {
+//   const posts = await postsService.getPosts();
+//   return {
+//     props: {
+//       posts,
+//     },
+//   };
+// }
 
 const HomePageContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 60%;
+`;
+
+const Main = styled.main`
+  width: 60%;
 `;
