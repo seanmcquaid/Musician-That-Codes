9e1076ba32f4243a49312767411e064a52691b9e
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-15 21:03:50 -0400

    commented out api calls for now

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,19 +1,26 @@
+import styled from 'styled-components';
 import postsService from '../../services/postsService';
 
-export async function getStaticPaths() {
-  const posts = await postsService.getPosts();
-
-  const paths = posts.map((post) => ({
-    params: { id: post.id },
-  }));
-
-  return { paths, fallback: false };
+export default function PostPage() {
+  return <PostPageContainer></PostPageContainer>;
 }
 
-export async function getStaticProps({ params }) {
-  const post = await postsService.getPost(params.id);
+const PostPageContainer = styled.div``;
 
-  return {
-    props: { post },
-  };
-}
+// export async function getStaticPaths() {
+//   const posts = await postsService.getPosts();
+
+//   const paths = posts.map((post) => ({
+//     params: { id: post.id },
+//   }));
+
+//   return { paths, fallback: false };
+// }
+
+// export async function getStaticProps({ params }) {
+//   const post = await postsService.getPost(params.id);
+
+//   return {
+//     props: { post },
+//   };
+// }
