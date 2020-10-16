f9276840839b2ceedbc1d248c7ca9784d49e29bc
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-16 18:42:42 -0400

    finished posts service

--- services/postsService.js
+++ services/postsService.js
@@ -5,10 +5,10 @@ import remark from 'remark';
 import html from 'remark-html';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
+const fileNames = fs.readdirSync(postsDirectory);
 
 export default {
   getSortedPosts: () => {
-    const fileNames = fs.readdirSync(postsDirectory);
     const allPostsData = fileNames.map((fileName) => {
       const id = fileName.replace(/\.md$/, '');
 
@@ -25,6 +25,30 @@ export default {
 
     return allPostsData.sort((a, b) => a.date < b.date);
   },
-  getAllPostIds: () => {},
-  getPost: () => {},
+  getAllPostIds: () => {
+    return fileNames.map((fileName) => {
+      return {
+        params: {
+          id: fileName.replace(/\.md$/, ''),
+        },
+      };
+    });
+  },
+  getPostDataById: async (id) => {
+    const fullPath = path.join(postsDirectory, `${id}.md`);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+    const matterResult = matter(fileContents);
+
+    const processedContent = await remark()
+      .use(html)
+      .process(matterResult.content);
+    const contentHtml = processedContent.toString();
+
+    return {
+      id,
+      contentHtml,
+      ...matterResult.data,
+    };
+  },
 };
