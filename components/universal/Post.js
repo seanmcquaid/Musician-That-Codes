df4ccd1ab35a80fc2ebe6a83cb6472235806ca57
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-16 18:29:11 -0400

    fleshed out Post component

--- components/universal/Post.js
+++ components/universal/Post.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { parseISO, format } from 'date-fns';
 
-const Post = React.memo(({ date, title, id }) => <StyledPost>Post</StyledPost>);
+const Post = React.memo(({ date, title, id }) => (
+  <StyledPost href={`/posts/${id}`}>
+    <PostTitle>{title}</PostTitle>
+    <PostDateTime dateTime={parseISO(date)}>
+      {format(date, 'LLLL d, yyyy')}
+    </PostDateTime>
+  </StyledPost>
+));
 
-const StyledPost = styled.div``;
+const StyledPost = styled(Link)``;
+
+const PostTitle = styled.h4``;
+
+const PostDateTime = styled.time``;
 
 Post.propTypes = {
   date: PropTypes.string.isRequired,
