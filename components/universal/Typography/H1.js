775b059ab0afb2f9917bdbbc79d607a5c579111e
seanmcquaid <sean.mcquaid2@verizon.net>
2020-10-18 21:46:13 -0400

    added google fonts

--- components/universal/Typography/H1.js
+++ components/universal/Typography/H1.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const H1 = React.memo(styled.h1``);
+const H1 = React.memo(styled.h1`
+  font-family: 'Rubik', sans-serif;
+`);
 
 export default H1;
--- components/universal/Typography/P.js
+++ components/universal/Typography/P.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const P = React.memo(styled.p``);
+const P = React.memo(styled.p`
+  font-family: 'Karla', sans-serif;
+`);
 
 export default P;
